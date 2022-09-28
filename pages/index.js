950f99605452da70078cad16e6cf08937525e64e
Ankit Solanki <ankit.solanki@multidots.com>
2022-09-28 18:57:57 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,21 @@ import Footer from './component/footer'
 // import Head from 'next/head'
 
 export default function Home(data) {
+
   return <>
             <Head>
-            <title>{data.yoast_head_json.title}</title>
-            <meta name="description" content={data.yoast_head_json.description} />
+            <title>{data.post.yoast_head_json.title}</title>
+            <meta name="description" content={data.post.yoast_head_json.description} />
             {/* <meta name="robots" content="noindex, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" /> */}
-            <meta property="og:locale" content={data.yoast_head_json.og_locale} />
-            <meta property="og:type" content={data.yoast_head_json.og_type} />
-            <meta property="og:title" content={data.yoast_head_json.og_title} />
-            <meta property="og:description" content={data.yoast_head_json.og_description} />
-            <meta property="og:url" content={data.yoast_head_json.og_url} />
-            <meta property="og:site_name" content={data.yoast_head_json.og_site_name} />
-            <meta property="article:modified_time" content={data.yoast_head_json.article_modified_time} />
-            <meta name="twitter:card" content={data.yoast_head_json.twitter_card} />
-            <meta name="twitter:image" content={data.yoast_head_json.twitter_image} />
+            <meta property="og:locale" content={data.post.yoast_head_json.og_locale} />
+            <meta property="og:type" content={data.post.yoast_head_json.og_type} />
+            <meta property="og:title" content={data.post.yoast_head_json.og_title} />
+            <meta property="og:description" content={data.post.yoast_head_json.og_description} />
+            <meta property="og:url" content={data.post.yoast_head_json.og_url} />
+            <meta property="og:site_name" content={data.post.yoast_head_json.og_site_name} />
+            <meta property="article:modified_time" content={data.post.yoast_head_json.article_modified_time} />
+            <meta name="twitter:card" content={data.post.yoast_head_json.twitter_card} />
+            <meta name="twitter:image" content={data.post.yoast_head_json.twitter_image} />
             <meta name="twitter:label1" content="Est. reading time" />
 
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
